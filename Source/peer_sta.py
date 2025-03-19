337dc491377ae96a75b75d5b144e610ca889e95e
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2025-03-19 16:24:23 +0700

    Implement system design code

--- Source/peer_sta.py
+++ Source/peer_sta.py
@@ -1,12 +1,16 @@
+import sys
 import time
 import mmap
 import socket
 import argparse
+import threading
 from threading import Thread
 
 # ===============================================================================================
-# ================BEGIN SERVER FUNCTION======================================
+# ================ SERVER FUNCTION ==============================================================
 # ===============================================================================================
+
+event = threading.Event()  # Create an event to sync thread
 
 # New peer connect to tracker
 def new_server_incoming(addr, conn):
@@ -42,7 +46,10 @@ def thread_server(hostip, port):
 
     # Put the socket into listen mode to wait for the client to connect.
     # Parameter 10 is the connection queue length, which means that up to 10 clients can wait before being rejected.
-    serversocket.listen(10)                                                                                                   
+    serversocket.listen(10)
+    print("Peer server listening on: {}:{}".format(hostip, port))
+    event.set()                                                     # Send signal for client thread
+
     while True:
         # It will wait (blocking) until a client connects.
         # When a client connects, the server creates a new socket (conn) to communicate with that client.
@@ -51,12 +58,9 @@ def thread_server(hostip, port):
         nconn = Thread(target=new_server_incoming, args=(addr, conn))
         nconn.start()                                               # Activate the thread
 
-# ===============================================================================================
-# ================END SERVER FUNCTION======================================
-# ===============================================================================================
 
 # ===============================================================================================
-# ================BEGIN CLIENT FUNCTION======================================
+# ================ CLIENT FUNCTION ==============================================================
 # ===============================================================================================
 
 def new_connection(tid, host, port):
@@ -82,17 +86,39 @@ def connect_server(threadnum, host, port):
     [t.join() for t in threads]
 
 def thread_client(id, serverip, serverport, peerip, peerport):
-    print('Thread ID {:d} connecting to {}:{:d}'.format(id, serverip, serverport))
-    """
-    Need to do here handle communication between tracker and peer
-    """
+    event.wait()                                                    # Wait signal for server thread
+
+    print('Client thread ID {:d} connecting to {}:{:d}'.format(id, serverip, serverport))
+
+    # client_socket = socket.socket()
+    # client_socket.connect((serverip, serverport))
+
+    # print('Client thread ID {:d} connect success to {}:{:d}'.format(id, serverip, serverport))
+
+    while True:
+        print_gui()
+        command = input("")
+
+        if command == "1":
+            function1()
+        elif command == "2":
+            function2()
+        elif command == "3":
+            function3()
+        elif command == "4":
+            function4()
+        elif command == "5":
+            function5()
+        elif command == "6":
+            sys.exit()
+        else:
+            print('Error function number, try again')
+            print_gui()
+            continue
+
 
 # ===============================================================================================
-# ================END CLIENT FUNCTION======================================
-# ===============================================================================================
-
-# ===============================================================================================
-# ================BEGIN AGENCY FUNCTION======================================
+# ================ AGENCY FUNCTION ==============================================================
 # ===============================================================================================
 
 def thread_agent(time_fetching, filepath):
@@ -122,12 +148,48 @@ def thread_agent(time_fetching, filepath):
 
     exit()
 
-# ===============================================================================================
-# ================END AGENCY FUNCTION======================================
-# ===============================================================================================
 
 # ===============================================================================================
-# ================MAIN FUCNTION======================================
+# ==============PEER TO TRACKER FUNCTION=========================================================
+# ===============================================================================================
+
+def function1():
+    print("Function 1 do")
+    return
+
+def function2():
+    print("Function 2 do")
+    return
+
+def function3():
+    print("Function 3 do")
+    return
+
+def function4():
+    print("Function 4 do")
+    return
+
+def function5():
+    print("Function 5 do")
+    return
+
+# ===============================================================================================
+# =========================== UX/UI =============================================================
+# ===============================================================================================
+
+def print_gui():
+    print("\nFUNCTION LIST")
+    print("1 - function 1")
+    print("2 - function 2")
+    print("3 - function 3")
+    print("4 - function 4")
+    print("5 - function 5")
+    print("6 - Exit")
+    print("Enter the function number: ")
+
+
+# ===============================================================================================
+# ================ MAIN FUCNTION ================================================================
 # ===============================================================================================
 
 if __name__ == "__main__":
@@ -138,12 +200,12 @@ if __name__ == "__main__":
     )
     parser.add_argument('--server-ip')
     parser.add_argument('--server-port', type=int)
-    parser.add_argument('--agent-path')
+    #parser.add_argument('--agent-path')
 
     args = parser.parse_args()
     serverip = args.server_ip
     serverport = args.server_port
-    agentpath = args.agent_path
+    #agentpath = args.agent_path
 
     peerip = get_host_default_interface_ip()
     peerport = 33357
@@ -154,8 +216,9 @@ if __name__ == "__main__":
 
     tserver.start()
     tclient.start()
+
     tclient.join()
     #tagent.start()
 
-    # Never completed
+    #Never completed
     tserver.join()
